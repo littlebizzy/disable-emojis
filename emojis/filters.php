7b808706244454714624c7ed6e1f80b93c7b1e8f
Pau <pauiglesias@gmail.com>
2018-01-11 17:23:13 +0100

    Remove filters using the parent class, remove tinyMCE plugin and remove emojis URLs from prefetching.

--- emojis/filters.php
+++ emojis/filters.php
@@ -13,10 +13,15 @@ class Filters extends Emojis {
 
 
 
+	// Properties
+	// ---------------------------------------------------------------------------------------------------
+
+
+
 	/**
 	 * Involved filters
 	 */
-	private $filters = [
+	protected $filters = [
 		'wp_staticize_emoji' => [
 			['the_content_feed', 10],
 			['comment_text_rss', 10],
@@ -29,17 +34,71 @@ class Filters extends Emojis {
 
 
 	/**
+	 * Partial matching URL
+	 */
+	protected $matchingURL = 's.w.org/images/core/emoji/'
+
+
+
+	// WP hooks
+	// ---------------------------------------------------------------------------------------------------
+
+
+
+	/**
 	 * Handle the WP init hook
 	 */
 	public function init() {
 
-		// Filters removed
-		remove_filter('the_content_feed', 	'wp_staticize_emoji');
-		remove_filter('comment_text_rss', 	'wp_staticize_emoji');
-		remove_filter('wp_mail', 			'wp_staticize_emoji_for_email');
+		// Remove filters
+		$this->remove('filters', $this->filters);
 
 		// Modifications
+		add_filter('tiny_mce_plugins',  [&$this, 'tinyMCEPlugins']);
+		add_filter('wp_resource_hints', [&$this, 'removeDNSPrefetch'], 10, 2);
+	}
 
+
+
+	/**
+	 * Handle tinyMCE supported plugins
+	 */
+	public function tinyMCEPlugins($plugins) {
+		return (empty($plugins) || !is_array($plugins))? [] : array_diff($plugins, array('wpemoji'));
+	}
+
+
+
+	/**
+	 * Remove emoji URL's from DNS prefetching hints
+	 */
+	public function removeDNSPrefetch($urls, $relation_type) {
+
+		// Avoid non-dns-prefetch cases
+		if ('dns-prefetch' != $relation_type)
+			return $urls;
+
+		// Initilize
+		$newURLs = [];
+
+		// Enum current values
+		foreach ($urls as $key => $value) {
+
+			// Check item
+			$url = $value
+			if (is_array($url)) {
+
+				// Copy attr
+				$url = empty($url['href'])? null : $url['href'];
+			}
+
+			// Add item if not contains coincidences
+			if (empty($url) || false === stripos($url, $this->matchingURL))
+				$newURLs[] = $value;
+		}
+
+		// Done
+		return $newURLs;
 	}
 
 
