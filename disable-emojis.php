a06f38c07617731a269a0a1d47a2bbc0ae51191a
Jesse Nickles <jesse@littlebizzy.com>
2018-04-05 13:53:35 -0700

    Update disable-emojis.php

--- disable-emojis.php
+++ disable-emojis.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Emojis
 Plugin URI: https://www.littlebizzy.com/plugins/disable-emojis
 Description: Completely disables both the old and new versions of WordPress emojis, removes the corresponding javascript calls, and improves page loading times.
-Version: 1.1.0
+Version: 1.1.1
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -14,6 +14,15 @@ Prefix: DSBEMJ
 // Plugin namespace
 namespace LittleBizzy\DisableEmojis;
 
+/**
+ * Admin Notices Multisite check
+ * Uncomment //return to disable this plugin on Multisite installs
+ */
+require_once dirname(__FILE__).'/admin-notices-ms.php';
+if (false !== \LittleBizzy\DisableEmojis\Admin_Notices_MS::instance(__FILE__)) {
+	//return;
+}
+
 // Block direct calls
 if (!function_exists('add_action'))
 	die;
@@ -21,7 +30,7 @@ if (!function_exists('add_action'))
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'dsbemj';
-const VERSION = '1.1.0';
+const VERSION = '1.1.1';
 
 // Loader
 require_once dirname(FILE).'/helpers/loader.php';
