70ce1829005a503f5b1711e5ad9e800f28db6635
Jesse Nickles <jesse@littlebizzy.com>
2024-10-24 15:54:02 +0700

    Update disable-emojis.php

--- disable-emojis.php
+++ disable-emojis.php
@@ -27,22 +27,22 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 add_action( 'init', function() {
     // remove emoji actions from the front-end
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
     
     // remove emoji actions from the admin area
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_styles', 'print_emoji_styles', 10 );
 
     // remove emoji actions from embedded content
-    remove_action( 'embed_head', 'print_emoji_detection_script' );
-    remove_action( 'embed_head', 'print_emoji_styles' );
+    remove_action( 'embed_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'embed_head', 'print_emoji_styles', 10 );
     
     // disable emojis in emails
-    remove_action( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    remove_action( 'wp_mail', 'wp_staticize_emoji_for_email', 10 );
 
     // disable emojis in RSS feeds
-    remove_action( 'the_content_feed', 'wp_staticize_emoji' );
-    remove_action( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_action( 'the_content_feed', 'wp_staticize_emoji', 10 );
+    remove_action( 'comment_text_rss', 'wp_staticize_emoji', 10 );
 
     // remove emoji support from TinyMCE (classic editor)
     add_filter( 'tiny_mce_plugins', function( $plugins ) {
@@ -50,52 +50,52 @@ add_action( 'init', function() {
             return array_diff( $plugins, [ 'wpemoji' ] );
         }
         return $plugins;
-    });
+    }, 10 );
 
     // disable DNS prefetch for emojis
-    add_filter( 'emoji_svg_url', '__return_false' );
+    add_filter( 'emoji_svg_url', '__return_false', 10 );
 
     // disable emoji scripts for XML-RPC requests (safety net)
     if ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {
         remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
     }
 
     // disable emoji scripts for REST API requests (safety net)
     if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
         remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-        remove_action( 'wp_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles', 10 );
     }
 
     // remove emojis in post titles (including RSS feed titles)
     add_filter( 'the_title', function( $title ) {
         $emoji_regex = '/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F1E6}-\x{1F1FF}\x{1F900}-\x{1F9FF}\x{1FA70}-\x{1FAFF}]/u';
         return preg_replace( $emoji_regex, '', $title );
-    });
+    }, 10 );
 
     // remove emojis in post content
     add_filter( 'the_content', function( $content ) {
         $emoji_regex = '/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F1E6}-\x{1F1FF}\x{1F900}-\x{1F9FF}\x{1FA70}-\x{1FAFF}]/u';
         return preg_replace( $emoji_regex, '', $content );
-    });
+    }, 10 );
 
     // remove emojis in post excerpts
     add_filter( 'the_excerpt', function( $excerpt ) {
         $emoji_regex = '/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F1E6}-\x{1F1FF}\x{1F900}-\x{1F9FF}\x{1FA70}-\x{1FAFF}]/u';
         return preg_replace( $emoji_regex, '', $excerpt );
-    });
+    }, 10 );
 
     // remove emojis in comment content
     add_filter( 'comment_text', function( $comment_text ) {
         $emoji_regex = '/[\x{1F600}-\x{1F64F}\x{1F300}-\x{1F5FF}\x{1F680}-\x{1F6FF}\x{2600}-\x{26FF}\x{2700}-\x{27BF}\x{1F1E6}-\x{1F1FF}\x{1F900}-\x{1F9FF}\x{1FA70}-\x{1FAFF}]/u';
         return preg_replace( $emoji_regex, '', $comment_text );
-    });
+    }, 10 );
 });
 
 // disable emoji scripts in block editor (Gutenberg)
 add_action( 'enqueue_block_editor_assets', function() {
-    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_styles', 'print_emoji_styles', 10 );
 });
 
 // Ref: ChatGPT
